f164880c8a4b764e9a3d6f3c1a8650be4644720e
Nicolas <nicolasdelfino585@gmail.com>
2025-03-01 19:32:55 -0300

    código reformulado con map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,29 +3,24 @@ import contacts from "../contacts";
 import Cards from "./Cards";
 import Avatar from "./Avatar";
 
+function createContacts(contacts) {
+  return (
+    <Cards
+      key={contacts.key}
+      nombre={contacts.name}
+      img={contacts.imgURL}
+      phone={contacts.phone}
+      email={contacts.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      {contacts.map(createContacts)}
       <Avatar img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" />
-      <Cards
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        phone={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Cards
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        phone={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Cards
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        phone={contacts[2].phone}
-        email={contacts[2].email}
-      />
     </div>
   );
 }
